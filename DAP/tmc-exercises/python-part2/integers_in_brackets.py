12b1bb17fe14e20f9d74a5623bbfe5a318da709b
soilangcon <soilangcon@gmail.com>
2024-05-28 10:31:27 +0700

    part02-e01_integers_in_brackets

---
+++ DAP/tmc-exercises/python-part2/integers_in_brackets.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import re
+
+def integers_in_brackets(s):
+    # pattern = r'\s*[0-9]+\s*'
+    # pattern = r'\s*[+-]?\d+\s*'
+    pattern = r'\[\s*([+-]?\d+)\s*\]'
+    matches = re.findall(pattern, s)
+    # matches = re.finditer(pattern, s)
+    # return matches, [match.group() for match in matches] 
+    return matches, list(map(int, matches)) 
+
+def main():
+    print(integers_in_brackets(" afd [asd] [12 ] [a34] [ -43 ]tt [+12]xxx"))
+
+if __name__ == "__main__":
+    main()
