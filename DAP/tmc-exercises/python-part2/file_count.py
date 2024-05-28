98bac1569f6c598ec48bf6bf5660d4ce3f45e2e2
soilangcon <soilangcon@gmail.com>
2024-05-28 15:44:35 +0700

    Add files via upload

---
+++ DAP/tmc-exercises/python-part2/file_count.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+def file_count(filename):
+    linecount, wordcount, lettercount = 0, 0, 0
+    with open(filename, 'r') as f:
+        for line in f:
+            linecount += 1
+            words = line.split()
+            wordcount += len(words)
+            lettercount += len(line)
+    return (linecount, wordcount, lettercount)
+
+def main():
+    for filename in sys.argv[1:]:
+        lines, words, chars = file_count(filename)
+        print(f"{lines}\t{words}\t{chars}\t{filename}")
+
+if __name__ == "__main__":
+    main()
