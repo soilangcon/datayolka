c60db5fa23b3a243f127b7292edb3fa0190d94b7
soilangcon <soilangcon@gmail.com>
2024-05-28 17:26:03 +0700

    Update part02-e15_vector_angles.py

--- DAP/tmc-exercises/numpy-part1/part02-e15_vector_angles.py
+++ DAP/tmc-exercises/numpy-part1/part02-e15_vector_angles.py
@@ -4,7 +4,6 @@ import scipy.linalg
 def vector_angles(X, Y):
     return np.degrees(np.arccos(np.clip(np.sum(X*Y, axis=1)/(np.linalg.norm(X, axis=1) * np.linalg.norm(Y, axis=1)), -1.0, 1.0)))
 
-
 def main():
     X = np.array([[1, 2, 3], [4, 5, 6]])
     Y = np.array([[7, 8, 9], [10, 11, 12]])
