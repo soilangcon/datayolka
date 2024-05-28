591f7c06ee0bf1d39d5bdb7a3f5bad14f824fe65
soilangcon <soilangcon@gmail.com>
2024-05-28 10:09:13 +0700

    Add files via upload

---
+++ DAP/tmc-exercises/python-part1/areas_of_shapes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import math
+
+def area_triangle():
+    base = int(input("Give base of the triangle: ")) 
+    height = int(input("Give height of the triangle: "))
+    return 0.5*height*base
+
+def area_rectangle():
+    width = int(input("Give width of the rectangle: "))
+    height = int(input("Give height of the rectangle: "))
+    return width*height 
+
+def area_circle():
+    radius = int(input("Give radius of the circle: "))
+    return math.pi*(radius**2)
+
+
+def main():
+    shapes = {"triangle": area_triangle, "rectangle": area_rectangle, "circle": area_circle}
+    while True:
+        usrin = input("Choose a shape (triangle, rectangle, circle): ")
+        if usrin in shapes.keys():
+            print(f"The area is {shapes[usrin]()}") 
+        elif usrin == "":
+            break 
+        else:
+            print("Unknown shape!")
+
+if __name__ == "__main__":
+    main()
