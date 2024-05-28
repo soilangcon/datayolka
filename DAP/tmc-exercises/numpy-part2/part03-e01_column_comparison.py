22089757303399ec637f7f60594fdcd43fe00c97
soilangcon <soilangcon@gmail.com>
2024-05-28 18:56:11 +0700

    Update part03-e01_column_comparison.py

--- DAP/tmc-exercises/numpy-part2/part03-e01_column_comparison.py
+++ DAP/tmc-exercises/numpy-part2/part03-e01_column_comparison.py
@@ -4,7 +4,12 @@ def column_comparison(a):
     return a[a[:, 1] > a[:, -2]]
     
 def main():
-    pass
+    a = np.array([[8, 9, 3, 8, 8],
+                  [0, 5, 3, 9, 9],
+                  [5, 7, 6, 0, 4],
+                  [7, 8, 1, 6, 2],
+                  [2, 1, 3, 5, 8]])
+    print(column_comparision(a))
 
 if __name__ == "__main__":
     main()
