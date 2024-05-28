fe3e406bfd9aceb1ac18adf15447c56967a36bdc
soilangcon <soilangcon@gmail.com>
2024-05-28 15:50:01 +0700

    Add files via upload

---
+++ DAP/tmc-exercises/python-part2/prepend.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+class Prepend(object):
+    def __init__(self, start):
+        self.start = start 
+    def write(self, prepend):
+        print(f"{self.start}{prepend}")
+
+def main():
+    p = Prepend("+++ ")
+    p.write("Hello")
+
+if __name__ == "__main__":
+    main()
